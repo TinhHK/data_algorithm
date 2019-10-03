0323e93fd664fe1eb4071846a8aafe87e2ec0fb9
tinhhk <tinhha@interspace.vn>
2019-10-03 19:00:57 +0700

    mutual recursion

--- recursive/mutual_recursion.php
+++ recursive/mutual_recursion.php
@@ -1,1 +1,16 @@
 <?php
+
+function isEvent($n): bool
+{
+	if($n == 0){
+		return true;
+	} else {
+		return isOdd($n-1);
+	}
+}
+function isOdd($n): bool
+{
+	return !isEvent($n);
+}
+
+var_dump(isEvent(6));
