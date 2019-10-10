644410889edfebbed1cc5d063f247d483382e959
tinhhk <tinhha@interspace.vn>
2019-10-10 08:46:18 +0700

    insertion sort

--- sort/insertion_sort.php
+++ sort/insertion_sort.php
@@ -5,7 +5,17 @@ function insertion_sort(array $arr): array
 {
 	// one element is in order
 	// loop from n = 1
-	// compare with n - 1 from the end of array to find position.
+	for($i = 1; $i< count($arr); $i++){
+		$currentEle = $arr[$i];
+		// compare with n - 1 from the end of array to find position.
+		for($pos = $i; $pos > 0 && $arr[$pos-1] > $currentEle; $pos-- ){
+			// move to right
+			$arr[$pos] = $arr[$pos -1];
+		}
+		// suitable position
+		$arr[$pos] = $currentEle;
+	}
+	return $arr;
 }
 $arr = createAnArray(8);
 print_array($arr);
